34559335c8d8cad7f27fc32dc74fc085b085fdd2
barcelona <mohit_nitd@outlook.com>
2021-12-07 08:46:04 -0600

    branch 3 validation p1

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,5 +1,7 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -9,17 +11,28 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private  String projectName;
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min = 4, max = 5, message = "please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
+
     private String projectIdentifier;
-    private  String  description;
+    @NotBlank(message = "Project description is required")
+
+    private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
-// Constructor
+    // Constructor
     public Project() {
     }
 
@@ -89,14 +102,15 @@ public class Project {
 
     //store date of object creation
     @PrePersist
-    protected  void onCreate(){
+    protected void onCreate() {
         this.created_At = new Date();
     }
+
     //store date of object update
     @PreUpdate
-    protected  void  onUpdate(){
-    this.updated_At = new Date();
-}
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
 
 
 }
