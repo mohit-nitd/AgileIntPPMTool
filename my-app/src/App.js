40762dc5cace84d242144dd3fb7bc5486bf0e132
barcelona <mohit_nitd@outlook.com>
2021-12-15 06:50:57 -0600

    add router@4.3.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,17 +4,23 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
 
 
-class App extends Component{
-render() {
-  return (
-    <div className="App">
-    <Header />
-      <Dashboard />
-    </div>
-  );
-}
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
